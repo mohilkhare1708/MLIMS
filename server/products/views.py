e1ba83a1a39ff2603d0a9ff60a15fa92c3f72654
mishrakeshav <keshav.sm@somaiya.edu>
2021-03-21 10:50:03 +0530

    update

--- server/products/views.py
+++ server/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 app_name = 'products'
@@ -6,3 +7,10 @@ app_name = 'products'
 def index(request):
     context = {}
     return render(request,'products/index.html', context)
+
+def view_categories(request):
+    categories = Category.objects.all()
+    context = {
+        'categories':categories,
+    }
+    return render(request,'products/view_categories.html', context)
