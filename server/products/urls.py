6ede7ac6215889f2841a8215d4635cbb3265f22b
mishrakeshav <keshav.sm@somaiya.edu>
2021-03-21 14:51:39 +0530

    update

--- server/products/urls.py
+++ server/products/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.index , name = 'index'),
     path('view-categories/', views.view_categories , name = 'view_categories'),
     path('view-brands/', views.view_brands , name = 'view_brands'),
+    path('view-agents/', views.view_agents , name = 'view_agents'),
+    path('view-products/', views.view_products , name = 'view_products'),
+    path('products/add/', views.add_product , name = 'add_product'),
+    path('products/fetch/', views.fetch_product , name = 'fetch_product'),
+
 ]
--- server/products/views.py
+++ server/products/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.core.serializers import serialize
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-
-from .models import Category,Brand
+from django.contrib.auth.models import User
+from .models import Category,Brand,WareHouse,ProductModel,Product
+from users.models import Profile
+import json 
 
 # Create your views here.
 app_name = 'products'
@@ -18,6 +23,7 @@ def view_categories(request):
         'categories':categories,
     }
     return render(request,'products/view_categories.html', context)
+
 @login_required
 def view_brands(request):
     brands = Brand.objects.all()
@@ -25,7 +31,79 @@ def view_brands(request):
         'brands':brands,
     }
     return render(request,'products/view_brands.html', context)
+
+@login_required
+def view_agents(request):
+    agents  = Profile.objects.all().filter(is_admin=False) 
+    context = {
+        'agents':agents,
+    }
+    return render(request,'products/view_agents.html', context)
     
+@csrf_exempt
+def add_product(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        print(data)
+        warehouse = get_object_or_404(WareHouse,pk=data['warehouse'])
+        category = get_object_or_404(Category,pk=data['category'])
+        company = get_object_or_404(Brand, pk=data['company'])
+        unique_identifier = data['unique_identifier']
+        product_model = get_object_or_404(ProductModel,pk=data['product_model'])
+        product = Product.objects.create(
+            is_listed = True ,
+            unique_identifier = unique_identifier,
+            warehouse=warehouse,
+            category = category,
+            company =company,
+            product_model = product_model
+        )
+        return JsonResponse({"added" : "ok"},safe=False)
+    else:
+        warehouses = list(WareHouse.objects.all())
+        category = list(Category.objects.all())
+        company = list(Brand.objects.all())
+        product_model = list(ProductModel.objects.all())
+        data = {
+            'warehouses' : serialize('json', warehouses),
+            "category" : serialize('json',category),
+            "company" : serialize('json',company),
+            "product_model" : serialize('json',product_model)
+        }
+        return JsonResponse(data, safe=False)
+
+@csrf_exempt
+def fetch_product(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        print(data)
+        unique_identifier = data['unique_identifier']
+        product = get_object_or_404(Product , unique_identifier = unique_identifier)
+        data = {
+            'name' : product.product_model.name ,
+            'description' : product.product_model.description,
+            'company' : product.company.name,
+            'MRP' : product.product_model.mrp,
+            'image' : product.product_model.image.url,
+            'warehouse' : product.warehouse.name,
+            'latitude' : product.warehouse.latitude,
+            'longitude':product.warehouse.longitude,
+            'region' : product.warehouse.region,
+            'buying_date' : str(product.buying_date),
+            'selling_date' : str(product.selling_date),
+        }
+        return JsonResponse(data, safe=False)
+
+@login_required
+def view_products(request):
+    products = Product.objects.all()
+    context = {
+        'products':products,
+    }
+    return render(request,'products/view_products.html', context)
+    
+
+
 def landingpage(request):
     context={}
     return render(request, 'landingpage.html', context)
