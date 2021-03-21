a424f70013c85128dbab006e8d23799e9f249c94
mishrakeshav <keshav.sm@somaiya.edu>
2021-03-21 08:18:02 +0530

    update products app admin.py

--- server/products/admin.py
+++ server/products/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from . import models
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id','name')
+    search_fields = ('id','name')
+    list_per_page = 20
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id','name')
+    search_fields = ('id','name')
+    list_per_page = 20
+
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'company', )
+    list_display_links = ('id', 'name', 'company')
+    search_fields = ('id', 'name', 'company')
+    list_per_page = 20
+
+class WareHouseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'region',)
+    list_display_links = ('id', 'name', 'region',)
+    search_fields = ('id', 'name', 'region',)
+    list_per_page = 20
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'unique_identifier','product_model', 'company', 'category', 'warehouse')
+    list_display_links = ('id', 'unique_identifier', 'product_model',)
+    search_fields = ('id', 'unique_identifier','product_model', 'company', 'category', 'warehouse')
+    list_per_page = 20
+
+admin.site.register(models.Brand , BrandAdmin)
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.ProductModel, ProductModelAdmin)
+admin.site.register(models.WareHouse, WareHouseAdmin)
+admin.site.register(models.Product, ProductAdmin)
--- server/products/models.py
+++ server/products/models.py
@@ -4,11 +4,17 @@ class Brand(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name 
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(default = 'default.jpg',upload_to = 'category')
+
+    def __str__(self):
+        return self.name 
 
 class ProductModel(models.Model):
     name = models.CharField(max_length=200)
@@ -17,24 +23,39 @@ class ProductModel(models.Model):
     mrp = models.DecimalField(max_digits=20, decimal_places=2, default=Decimal(0.00))
     image = models.ImageField(default = 'default.jpg',upload_to = 'products')
 
+    def __str__(self):
+        return self.name
+
 class WareHouse(models.Model):
     name = models.CharField(max_length = 200)
     description = models.TextField()
     region = models.CharField(max_length=200)
     address1 = models.CharField(max_length = 300)
     address2 = models.CharField(max_length=500)
-    latitude =  models.DecimalField(max_digits=20, decimal_places=8, default=Decimal(0.00))
-    longitude = models.DecimalField(max_digits=20, decimal_places=8, default=Decimal(0.00))
+    latitude =  models.DecimalField(max_digits=20, decimal_places=8, default=0.0)
+    longitude = models.DecimalField(max_digits=20, decimal_places=8, default=0.0)
+
+    def __str__(self):
+        return self.name 
 
 class Product(models.Model):
-    buying date = models.DateTimeField()
-    selling date = models.DateTimeField()
+    buying_date = models.DateTimeField()
+    selling_date = models.DateTimeField()
     is_sold = models.Boolean(default = False)
-
+    is_listed = models.BooleanField(default=False)
+    unique_identifier = models.CharField(max_length = 300, unique=True)
     # Foreign Keys 
     warehouse = models.ForeignKey(WareHouse , on_delete = models.SET_NULL , null = True , blank = True)
     category = models.ForeignKey(Category , on_delete = models.SET_NULL , null = True , blank = True)
     company = models.ForeignKey(Brand , on_delete = models.SET_NULL , null = True , blank = True)
     product_model = models.ForeignKey(ProductModel , on_delete = models.SET_NULL , null = True , blank = True)
+
+    def __str__(self):
+        if self.product_model:
+            return f"{self.product_model} {self.unique_identifier}"
+
+        return f"{self.unique_identifier}"
+         
+
 
 
